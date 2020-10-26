c620b32fb3c89106c64f10a6ca95b2078010c35f
millennIumAMbiguity <37588844+millennIumAMbiguity@users.noreply.github.com>
2020-10-26 14:03:29 +0100

    Created AI

---
+++ Assets/Scripts/AI.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class AI : MonoBehaviour
+{
+
+    NavMeshAgent agent;
+    GameObject[] go;
+
+
+    void Start() {
+        agent = GetComponent<NavMeshAgent>();
+        go = GameObject.FindGameObjectsWithTag("Player");
+    }
+
+    private void FixedUpdate() {
+        float dist = 99999;
+        foreach (GameObject item in go) {
+            float d = Vector3.Distance(transform.position, item.transform.position);
+            if (d < dist) {
+                dist = d;
+                agent.SetDestination(item.transform.position);
+            }
+        }
+        if (dist < 2f) {
+            agent.isStopped = true;
+        } else {
+            agent.isStopped = false;
+        }
+    }
+
+
+}
