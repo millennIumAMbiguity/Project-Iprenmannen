20761b15b700a94b57028bacd2f24090c7986441
Axel Eriksson <a.eriksson0303@gmail.com>
2020-10-28 11:05:28 +0100

    Implemented Main Menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+	public void PlayGame()
+	{
+		//TODO: Fix scene setup
+		//SceneManager.LoadScene("GameScene");
+		Debug.Log("Play Game");
+	}
+
+	public void ExitGame()
+	{
+#if UNITY_EDITOR
+		Debug.LogWarning("Could not exit application because is editor.");
+#else
+		Application.Quit();
+#endif
+		
+	}
+}
